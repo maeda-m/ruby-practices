9b721161a4b065138f0e328f24d2083743367b3c
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2022-03-29 16:55:37 +0900

    スコア計算の実行に関するテストとREADMEを追加した

--- 04.bowling/README.md
+++ 04.bowling/README.md
@@ -4,8 +4,18 @@
 
 ```
 $ cd ./ruby-practices
-$ ./04.bowling/bowling.rb
+$ ./04.bowling/bowling.rb 1ゲーム分の投球記録
 ```
+
+### 1ゲーム分の投球記録について
+
+e.g. `6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5`
+
+|表記  |意味        |
+|:-----|:-----------|
+|`,`   |投球の区切り|
+|`0-10`|何本倒したか|
+|`X`   |ストライク  |
 
 ## テスト方法
 
@@ -18,5 +28,5 @@ $ ruby ./04.bowling/test_bowling.rb
 
 ```
 $ cd ./ruby-practices
-$ bundle exec rubocop
+$ rubocop
 ```
--- 04.bowling/test_bowling.rb
+++ 04.bowling/test_bowling.rb
@@ -1,1 +1,36 @@
 # frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative 'bowling'
+
+describe Bowling::Game do
+  it 'スコアは 139 であること' do
+    game = Bowling::Game.parse('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
+    assert_equal 139, game.score
+  end
+
+  it 'スコアは 164 であること' do
+    game = Bowling::Game.parse('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
+    assert_equal 164, game.score
+  end
+
+  it 'スコアは 107 であること' do
+    game = Bowling::Game.parse('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
+    assert_equal 107, game.score
+  end
+
+  it 'スコアは 134 であること' do
+    game = Bowling::Game.parse('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
+    assert_equal 134, game.score
+  end
+
+  it 'スコアは 144 であること' do
+    game = Bowling::Game.parse('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8')
+    assert_equal 144, game.score
+  end
+
+  it 'スコアは 300 であること' do
+    game = Bowling::Game.parse('X,X,X,X,X,X,X,X,X,X,X,X')
+    assert_equal 300, game.score
+  end
+end
