fe050051ec3dbc8c7e7a412d96044c73ea191e37
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2022-04-10 14:42:37 +0900

    requireは一番上
    https://docs.komagata.org/5692

--- 05.ls/lib/list.rb
+++ 05.ls/lib/list.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
+require_relative 'option'
+require_relative 'command'
+
 module List
   class NotFoundOrAccessDeniedError < StandardError
   end
 end
-
-require_relative 'option'
-require_relative 'command'
