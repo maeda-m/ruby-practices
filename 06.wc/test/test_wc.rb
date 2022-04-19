8bfe166ff971ab35a7d9fc1a1bc3f073a24f6ffc
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2022-04-19 12:06:41 +0900

    wc コマンドを暫定実装した

--- 06.wc/test/test_wc.rb
+++ 06.wc/test/test_wc.rb
@@ -1,8 +1,120 @@
 # frozen_string_literal: true
 
 # rubocop:disable Metrics/BlockLength
+# rubocop:disable Layout/HeredocIndentation
 
 require 'minitest/autorun'
 require_relative '../wc'
 
+describe 'main' do
+  before do
+    Dir.chdir(File.expand_path('../../05.ls', __dir__))
+  end
+
+  describe '単数ファイル指定（wc lib/command.rb）' do
+    it '-l なし' do
+      stdout = <<~STDOUT
+      \  18  35 433 lib/command.rb
+      STDOUT
+
+      paths = %w[lib/command.rb]
+      assert_output(stdout) { main(paths, without_byte_and_word: false) }
+    end
+
+    it '-l あり' do
+      stdout = <<~STDOUT
+      \  18 lib/command.rb
+      STDOUT
+
+      paths = %w[lib/command.rb]
+      assert_output(stdout) { main(paths, without_byte_and_word: true) }
+    end
+  end
+
+  describe '複数ファイル指定（wc lib/option.rb lib/core.rb lib/list.rb）' do
+    it '-l なし' do
+      stdout = <<~STDOUT
+      \  47   98 1108 lib/option.rb
+      \  34   63  814 lib/core.rb
+      \   9   15  159 lib/list.rb
+      \  90  176 2081 合計
+      STDOUT
+
+      paths = %w[lib/option.rb lib/core.rb lib/list.rb]
+      assert_output(stdout) { main(paths, without_byte_and_word: false) }
+    end
+
+    it '-l あり' do
+      stdout = <<~STDOUT
+      \  47 lib/option.rb
+      \  34 lib/core.rb
+      \   9 lib/list.rb
+      \  90 合計
+      STDOUT
+
+      paths = %w[lib/option.rb lib/core.rb lib/list.rb]
+      assert_output(stdout) { main(paths, without_byte_and_word: true) }
+    end
+  end
+
+  describe 'ワイルドカードでのファイル指定（wc lib/*.rb）' do
+    it '-l なし' do
+      stdout = <<~STDOUT
+      \  18   35  433 lib/command.rb
+      \  34   63  814 lib/core.rb
+      \   9   15  159 lib/list.rb
+      \  47   98 1108 lib/option.rb
+      \ 108  211 2514 合計
+      STDOUT
+
+      paths = %w[lib/*.rb]
+      assert_output(stdout) { main(paths, without_byte_and_word: false) }
+    end
+
+    it '-l あり' do
+      stdout = <<~STDOUT
+      \  18 lib/command.rb
+      \  34 lib/core.rb
+      \   9 lib/list.rb
+      \  47 lib/option.rb
+      \ 108 合計
+      STDOUT
+
+      paths = %w[lib/*.rb]
+      assert_output(stdout) { main(paths, without_byte_and_word: true) }
+    end
+  end
+
+  describe 'パイプなしの標準入力' do
+    before do
+      stdin = <<~STDIN
+      1 2	3　4
+      5  6\t\t\t7　　8
+
+      \t
+      9
+      STDIN
+
+      $stdin = StringIO.new(stdin)
+    end
+
+    it '-l なし' do
+      stdout = <<~STDOUT
+      \   5   9 31
+      STDOUT
+
+      assert_output(stdout) { main([], without_byte_and_word: false) }
+    end
+
+    it '-l あり' do
+      stdout = <<~STDOUT
+      \   5
+      STDOUT
+
+      assert_output(stdout) { main([], without_byte_and_word: true) }
+    end
+  end
+end
+
+# rubocop:enable Layout/HeredocIndentation
 # rubocop:enable Metrics/BlockLength
--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,2 +1,82 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
+require 'optparse'
+require 'stringio'
+
+Message = Struct.new(:message)
+Row = Struct.new(:line_count, :word_count, :byte_count, :note)
+
+def main(patterns, without_byte_and_word:)
+  rows = []
+  if patterns.empty?
+    io = StringIO.new(readlines.join)
+    rows << generate_row(io, without_byte_and_word)
+  else
+    patterns.each do |pattern|
+      Dir.glob(pattern).each do |path|
+        file = File.open(path, 'r')
+        rows << generate_row(file, without_byte_and_word, path)
+      end
+    end
+  end
+
+  if rows.count >= 2
+    rows_without_message = rows.reject { |row| row.is_a?(Message) }
+    line_count = rows_without_message.map(&:line_count).compact.sum
+
+    footer = Row.new(line_count, nil, nil, '合計')
+    footer.word_count = rows_without_message.map(&:word_count).compact.sum unless without_byte_and_word
+    footer.byte_count = rows_without_message.map(&:byte_count).compact.sum unless without_byte_and_word
+
+    rows << footer
+  end
+
+  rows = adjust_column(rows)
+  puts rows.join("\n")
+end
+
+def generate_row(file, without_byte_and_word, path = nil)
+  content = file.read
+
+  lines = content.split("\n")
+  words = lines.map { |line| line.split(/[[:blank:]]+/) }.flatten
+  words = words.compact.reject(&:empty?)
+  bytes = content.chars.map(&:bytesize)
+
+  row = Row.new(lines.count)
+  row.word_count = words.count unless without_byte_and_word
+  row.byte_count = bytes.sum unless without_byte_and_word
+  row.note = path if path
+
+  row
+ensure
+  file.close
+end
+
+def adjust_column(rows_with_message)
+  rows = rows_with_message.reject { |row| row.is_a?(Message) }
+  max_line_width = [4, rows.map(&:line_count).max.to_s.size, 4].max
+  max_word_width = [2, rows.map(&:word_count).compact.max.to_s.size].max + 1
+  max_byte_width = rows.map(&:byte_count).compact.max.to_s.size
+
+  rows_with_message.map do |row|
+    if row.is_a?(Message)
+      row.message
+    else
+      [
+        row.line_count.to_s.rjust(max_line_width),
+        row.word_count.to_s.then { |s| s.empty? ? nil : s.rjust(max_word_width) },
+        row.byte_count.to_s.then { |s| s.empty? ? nil : s.rjust(max_byte_width) },
+        row.note
+      ].compact.reject(&:empty?).join(' ')
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  lines_option = ARGV.getopts('l').values.first
+  paths = ARGV
+
+  main(paths, without_byte_and_word: lines_option)
+end
